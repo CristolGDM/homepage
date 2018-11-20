3953b6785b3ea233004ab0c0276bf75e0df7926c
Cristol Grosdemouge <cristol.grosdemouge@brands4friends.jp>
2018-11-20 17:45:15 +0900

    Adds arrow to navigate between previous/next images

--- js/ImageService.js
+++ js/ImageService.js
@@ -26,6 +26,8 @@ var ImageServiceProvider = angular.module('ImageServiceProvider', [])
 			getAllImages: getAllImages,
 			getCurrentImage: getCurrentImage,
 			hideFullScreen: hideFullScreen,
+			selectNextImage: selectNextImage,
+			selectPreviousImage: selectPreviousImage,
 			setCurrentImage: setCurrentImage,
 			shouldShowFullscreen: shouldShowFullscreen,
 			showFullScreen: showFullScreen
@@ -43,6 +45,48 @@ var ImageServiceProvider = angular.module('ImageServiceProvider', [])
 		function hideFullScreen(){
 			currentImage = {};
 			fullScreenShown = false;
+		}
+
+		function selectNextImage(){
+			var nextImage;
+
+			for (var i = 0; i < allImages.length; i++) {
+				if(allImages[i].src === currentImage.src) {
+					if (i === allImages.length -1) {
+						nextImage = allImages[0];
+						break;
+					}
+					else {
+						nextImage = allImages[i+1];
+						break;
+					}
+				}
+			}
+
+			if(nextImage != null) {
+				setCurrentImage(nextImage.src, nextImage.caption);
+			}
+		}
+
+		function selectPreviousImage(){
+			var previousImage;
+
+			for (var i = 0; i < allImages.length; i++) {
+				if(allImages[i].src === currentImage.src) {
+					if (i === 0) {
+						previousImage = allImages[allImages.length -1];
+						break;
+					}
+					else {
+						previousImage = allImages[i-1];
+						break;
+					}
+				}
+			}
+
+			if(previousImage != null) {
+				setCurrentImage(previousImage.src, previousImage.caption);
+			}
 		}
 
 		function setCurrentImage(src, caption) {
