68a2c160cb4d003f5aba02defa863429153737cb
Cristol Grosdemouge <cristol.grosdemouge@brands4friends.jp>
2018-12-05 18:14:37 +0900

    Better scrolling from footer

--- js/MainCtrl.js
+++ js/MainCtrl.js
@@ -36,11 +36,26 @@ var MainController = function($scope, $transitions){
 		}
 	}
 
-	function scrollBackTop(){
-		window.scrollTo({
-			top: 0,
-			left: 0,
-			behavior: "smooth"
-		})
+	function scrollBackTop() {
+		var duration = 400;
+
+		scrollToX(document.body, document.body.scrollTop, document.body.offsetTop, 0, 1/duration, 20, easeOutCuaic);
+	}
+
+	function scrollToX(element, xFrom, xTo, t01, speed, step, motion) {
+		if (t01 < 0 || t01 > 1 || speed<= 0) {
+			element.scrollTop = xTo;
+			return;
+		}
+		element.scrollTop = xFrom - (xFrom - xTo) * motion(t01);
+		t01 += speed * step;
+
+		setTimeout(function() {
+			scrollToX(element, xFrom, xTo, t01, speed, step, motion);
+		}, step);
+	}
+	function easeOutCuaic(t){
+		t--;
+		return t*t*t+1;
 	}
 }
