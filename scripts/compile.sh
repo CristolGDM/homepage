ad660271596243d4a138df892f1a8f990e3d55e1
Cristol Grosdemouge <cristol.grosdemouge@brands4friends.jp>
2019-09-12 17:51:16 +0900

    Compile CSS for both sites at the same time; use common files

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,3 +1,4 @@
 r.js -o build-px.js
 r.js -o ../cristolgdm/scripts/build-cgm.js
 r.js -o cssIn=../css/style.css out=../style-built.css
+r.js -o cssIn=../cristolgdm/css/style.css out=../cristolgdm/style-built.css
