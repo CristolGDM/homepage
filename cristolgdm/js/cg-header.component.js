463ee8455104e688a59a97807ae89fbbe0dcae65
Cristol Grosdemouge <cristol.grosdemouge@brands4friends.jp>
2019-09-19 18:02:24 +0900

    Load smaller BG for headers in articles too

--- cristolgdm/js/cg-header.component.js
+++ cristolgdm/js/cg-header.component.js
@@ -18,15 +18,26 @@ var headerComponent = {
 function headerComponentController(){
 	/************** LOCAL VARIABLES **************/
 	var view = this;
+	var isSmallScreen = getComputedStyle(document.getElementById("js-mobile-detection")).display !== "none";
+	var isReallySmallScreen = window.innerWidth < 500;
 
 	/************** SCOPE VARIABLES **************/
 
 	/************** SCOPE FUNCTIONS **************/
 	view.$onInit = onInit;
+	view.getArticleBackground = getArticleBackground;
 
 	/***************** LISTENERS *****************/
 
 	/********** FUNCTIONS DECLARATIONS ***********/
+	function getArticleBackground(){
+		var fileName = "header";
+		if (isReallySmallScreen) fileName += "_portrait";
+		else if (isSmallScreen) fileName += "_small";
+
+		return 'articles/'+ view.id +'/' + fileName + '.jpg';
+	}
+
 	function onInit(){
 		var thisId = view.id;
 		var articles = varData.articles;
