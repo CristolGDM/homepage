f5c8c2494346aa0c4148afb6b16f340f82665084
DESKTOP-7ABRJIV\Cristol <cristol.gdm@gmail.com>
2023-04-09 23:09:57 +0900

    Add docker compose

--- start-both.sh
+++ start-both.sh
@@ -1,7 +1,2 @@
 npm run build
-docker rm pixelbreath -f
-docker rm cristolgdm -f
-docker build ./dist-pixel -f Dockerfile -t pixel
-docker build ./dist-cgdm -f Dockerfile -t cristol
-docker run -it --rm -d --name pixelbreath -p 8888:80 pixel
-docker run -it --rm -d --name cristolgdm -p 8889:80 cristol
+docker compose up -d
